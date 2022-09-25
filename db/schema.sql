5ebee75230d540e7a7b86d0fac8d2d193a4feed4
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-09-24 21:05:16 -0400

    set up handlebars as template engine

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS basic_user_system_db;
+
+CREATE DATABASE basic_user_system_db;
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+
+const routes = require('./controllers/')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'))
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
