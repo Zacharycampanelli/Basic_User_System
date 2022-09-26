6a57d876746e846a795aa5b5ad28a30cac5bdc72
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-09-25 22:36:49 -0400

    links to login and signup page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,14 +3,15 @@ const { User } = require('../../models');
 
 // GET all users
 router.get('/', (req, res) => {
-    User.findAll({
-        attributes: { exclude: ['password'] }
-    })
-    .then(dbUserData) {
-        if(!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id '})
-            return;
-        }
-    }
+  User.findAll({
+    attributes: { exclude: ['password'] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .caatch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-})
+
+module.exports = router;
